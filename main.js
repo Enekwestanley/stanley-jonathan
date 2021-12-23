4393f924230dbbd40b25bf924a0ed0022c65a3dd
Brimanz <brimanz2014@gmail.com>
2021-12-23 18:01:58 -0400

    a little animation for our names

---
+++ main.js
@@ -0,0 +1,10 @@
+const typed = new Typed('.typed', {
+    strings: ['Stanley & Jonathan'],
+    typedSpeed: 90,
+    backSpeed: 90,
+    startDelay: 50,
+    backDelay: 85,
+    loop: true,
+    showCursor: true,
+    cursorChar: '|',
+});
